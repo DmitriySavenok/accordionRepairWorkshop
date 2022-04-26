af4478a8c757b09b2246e18317e4f213bb4e15bd
Савенок Дмитрий <36520766+DmitriySavenok@users.noreply.github.com>
2022-04-26 11:59:13 +0300

    include botstrap

--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -7,6 +7,18 @@ let button = document.querySelector("#navigation-toggle");
 let burderIcon = document.querySelector(".menu-icon");
 let closeIcon = document.querySelector(".close-icon");
 
+$(document).ready(function() {
+  jQuery(".scrollto").click(function () {
+  elementClick = jQuery(this).attr("href")
+
+  navigationClose();
+
+  destination = jQuery(elementClick).offset().top - 70;
+  jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 1000);
+  return false;
+  });
+});
+
 function navigationClose() {
   button.classList.remove('opened');
   burderIcon.classList.remove('icon-hide');
