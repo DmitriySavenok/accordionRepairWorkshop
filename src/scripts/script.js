623318a5ad084053bd9bc8e2b37532caf75783db
Савенок Дмитрий <36520766+DmitriySavenok@users.noreply.github.com>
2022-05-20 18:26:42 +0300

    lighthouse optimization

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,4 @@
-AOS.init();
+AOS.init({
+  once: true, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+});
